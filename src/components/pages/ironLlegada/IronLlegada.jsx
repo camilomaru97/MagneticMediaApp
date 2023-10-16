da21cfab334d5605e5d0a3561a0a86a0b8abba2f
Angie Hernandez <avhernandez16@ucatolica.edu.co>
2023-10-16 17:35:55 -0500

    maquetacion iron llegada ironLlegada.jsx

--- src/components/pages/ironLlegada/IronLlegada.jsx
+++ src/components/pages/ironLlegada/IronLlegada.jsx
@@ -1,12 +1,52 @@
 import { MenuOpciones } from "../../../ui/MenuOpciones"
 import { Sidebar } from "../../../ui/Sidebar"
+import { Filter } from '../../helpers/Filter';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import '../../../styles/components/ironLlegada.css';
+import { Paginador } from '../../helpers/Paginador';
+
 
 export const IronLlegada = () => {
+  
+
+
   return (
     <div className="container">
       <Sidebar />
       <main>
-        <h1>Iron Llegada</h1>
+        <div className="ironLlegada_add">
+            <h1>Iron Llegada</h1>
+            <span
+                
+                className="material-symbols-outlined add"
+              >
+                add_circle
+              </span>
+          </div>
+          <Filter 
+          
+          />
+          <table className="ironLlegada_table">
+          <thead>
+            <tr>
+              <th>Tipo Transporte</th>
+              <th>Destino</th>
+              <th>Ubicacion</th>
+              <th>Numero Remision</th>
+              <th>Codigo Medio</th>
+              <th
+                style={{ cursor: 'pointer' }}
+              >Fecha</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+          
+        </table>
+        <Paginador />
       </main>
       <MenuOpciones />
     </div>
